4259395d67dc62048bdcbc8019d8f13faa3de045
taeginote <ab315601@naver.com>
2023-07-16 17:22:35 +0900

    📚Ts : 학습중

---
+++ functionType.ts
@@ -0,0 +1,13 @@
+{
+	//함수 선언식
+	function test4(a: number, b: number): number {
+		return a + b
+	}
+	test4(1, 3)
+
+	//함수 표현식
+	function test5(a: number, b: number): number {
+		return a + b
+	}
+	test5(1, 3)
+}
